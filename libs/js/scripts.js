f3c869d4dad2e4f376966ae8e822139be6bcdfed
Scarnface <scarnface@tutanota.com>
2021-07-11 11:41:12 +0100

    New JS animated menu

---
+++ libs/js/scripts.js
@@ -0,0 +1,17 @@
+/* ================================= 
+  Scripts
+==================================== */
+
+// Hide the menu on page load
+document.addEventListener('DOMContentLoaded', function() {
+  $(this).find('.menu-content').hide();
+}, false)
+
+// Switch menu visibilty on hover
+$('.menu').hover(function() {
+  $(this).find('#menu-btn-primary').hide();
+  $(this).find('.menu-content').show();
+}, function() {
+  $(this).find('.menu-content').hide();
+  $(this).find('#menu-btn-primary').show();
+});
