1e6c3c9ae9dd6fe1052ae75ec8c0ab304469045f
Scarnface <scarnface@tutanota.com>
2021-07-17 15:55:02 +0100

    minor fixes

--- libs/js/scripts.js
+++ libs/js/scripts.js
@@ -5,13 +5,14 @@
 //-----------------------------------------Array of gallery data -----------------------------------------------
 // n = Image ID, sw = Scale width, sh = Scale height, sx = Scale 'left', sy = Scale 'top', d = degrees to rotate
 //--------------------------------------------------------------------------------------------------------------
-const galleryImageData = [
+let galleryImageData = [
   { n: '#gI1', sw: 11.8, sh: 16.2, sx: 3.8, sy: 9.4, d: 0 }, // Rick's Garage Sale
   { n: '#gI2', sw: 11.3, sh: 16.2, sx: 40.2, sy: 10.1, d: -1 }, // Netmatters
   { n: '#gI3', sw: 12.3, sh: 16.4, sx: 83.5, sy: 9, d: -2 }, // Multi Api
   { n: '#gI4', sw: 11.6, sh: 16.2, sx: 47.2, sy: 44, d: 0 }, // Music Stache
   { n: '#gI5', sw: 11.6, sh: 16.6, sx: 26.3, sy: 74.1, d: -0.8 }, // blueJAM
-  { n: '#gI6', sw: 11.8, sh: 16.7, sx: 71.3, sy: 76.3, d: -0.5 }  // Codestrips
+  { n: '#gI6', sw: 11.8, sh: 16.7, sx: 71.3, sy: 76.3, d: -0.5 },  // Codestrips
+  { n: '#project-info', sw: 35, sh: 26, sx: 5, sy: 38, d: 0 }  // Dynamic info panel
 ];
 
 // Calculate the size and position of an absolute element in relation to its relative dynamic parent
