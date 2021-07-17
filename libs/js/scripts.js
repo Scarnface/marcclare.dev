2a9113bd534874ae7dd766ed5befa726aafbd177
Scarnface <scarnface@tutanota.com>
2021-07-18 00:48:59 +0100

    Code refactoring. Dynamic font size for gallery info text

--- libs/js/scripts.js
+++ libs/js/scripts.js
@@ -5,7 +5,7 @@
 //-----------------------------------------Array of gallery data -----------------------------------------------
 // n = Image ID, sw = Scale width, sh = Scale height, sx = Scale 'left', sy = Scale 'top', d = degrees to rotate
 //--------------------------------------------------------------------------------------------------------------
-let galleryImageData = [
+const galleryImageData = [
   { n: '#gI1', sw: 11.8, sh: 16.2, sx: 3.8, sy: 9.4, d: 0 }, // Rick's Garage Sale
   { n: '#gI2', sw: 11.3, sh: 16.2, sx: 40.2, sy: 10.1, d: -1 }, // Netmatters
   { n: '#gI3', sw: 12.3, sh: 16.4, sx: 83.5, sy: 9, d: -2 }, // Multi Api
@@ -15,33 +15,36 @@ let galleryImageData = [
   { n: '#project-info', sw: 37, sh: 26, sx: 3, sy: 38, d: 0 }  // Dynamic info panel
 ];
 
-// Calculate the size and position of an absolute element in relation to its relative dynamic parent
-let scaleToBg = function(array) {
-  // Calculate the width and height of the parent at the present time
+// Calculate the size and position of an absolute element in relation to its relative dynamic parent.
+let scaleToBg = array => {
+  // Calculate the width and height of the parent at the present time.
   let bgWidth = $('#gallery-background').width();
   let bgHeight = $('#gallery-background').height();
 
-  // Loop the array of images
+  // Loop the array of images.
   $.each(array, (key, value) => {
-    // Calculate the new size
+    // Calculate the new size.
     let newWidth = value.sw * bgWidth / 100;
     let newHeight = value.sh * bgHeight / 100;
-    // Calculate the new position
+    // Calculate the new position.
     let newLeft = value.sx * bgWidth / 100;
     let newTop = value.sy * bgHeight / 100;
-    // Assign the new values
+    // Calculate a corresponding font size for the info div
+    let fSize = newHeight / 5;
+    // Assign the new values.
     $(value.n).width(newWidth);
     $(value.n).height(newHeight);
     $(value.n).css({'top': newTop, 'left': newLeft, 'transform': 'rotate(' + value.d + 'deg)'});
+    $('#project-info').css('fontSize', fSize);
   });
 };
 
-// Run on initial page load
+// Run on initial page load.
 $(window).load(function() {
   scaleToBg(galleryImageData);
 });
 
-// Run on resize
+// Run on resize.
 $(window).resize(function() {
   scaleToBg(galleryImageData);
 });
@@ -49,19 +52,19 @@ $(window).resize(function() {
 //-----------------------------------------Array of project data -----------------------------------------------
 // t = Project title, d = Project description
 //--------------------------------------------------------------------------------------------------------------
-let projectData = [
+const projectData = [
   { t: 'Rick\'s Garage Sale', d: 'A high fidelity prototype of a web store, built in Axure.' },
   { t: 'Netmatters', d: 'A full front end clone of the Netmatters corporate site using SCSS for full browser compatability.' },
   { t: 'Multi API', d: 'Geonames APIs parsed dynamically to an HTML web page with AJAX, PHP and CURL.' },
   { t: 'Music Stache', d: 'HTML, CSS, JavaScript, and Handlebars to make a music store frontend.' },
   { t: 'BLUEjam', d: 'A REACT application for creating and storing custom Spotify playlists.' },
   { t: 'Codestrips', d: 'A comic strip app using Express, SQLite, and the SQLite3 node module.' },
-  { t: '', d: '' } // Clearing string
 ];
 
-// Create the paragraph element, load the typewriter plugin and pass the corresponding text to it
+// Create the paragraph element, load the typewriter plugin and pass the corresponding text to it.
 let typeText = obj => {
-  $( '#project-info' ).append( '<p id="project-description"></p>' );
+  // Create the HTML element to hold the description. Neccessary as we delete the element to stop the function on mouse out.
+  $('#project-info').append('<p id="project-description"></p>');
 
   let tText = document.getElementById('project-description');
 
@@ -70,46 +73,39 @@ let typeText = obj => {
       loop: false
   });
 
-  // Type the project title
+  // Type the project title.
   typewriter.typeString(obj.t)
       .pauseFor(2500)
       .deleteAll()
-      // Type the project description
+      // Type the project description.
       .typeString(obj.d)
       .pauseFor(2500)
       .deleteAll()
-      // CTA
+      // CTA.
       .typeString('Click the image to view!')
       .pauseFor(2500)
       .start();
 };
 
-// Mouse in events to pass the correct text for each image
-$( '#gI1' ).mouseenter(function() {
-  typeText(projectData[0])
+// Mouse in event to pass the correct text for each image.
+$('.portfolio-image').mouseenter(function() {
+  if( this.id === 'gI1' ) {
+    typeText(projectData[0]);
+  } else if( this.id === 'gI2' ) {
+    typeText(projectData[1]);
+  } else if( this.id === 'gI3' ) {
+    typeText(projectData[2]);
+  } else if( this.id === 'gI4' ) {
+    typeText(projectData[3]);
+  } else if( this.id === 'gI5' ) {
+    typeText(projectData[4]);
+  } else if( this.id === 'gI6' ) {
+    typeText(projectData[5]);
+  }
 });
 
-$( '#gI2' ).mouseenter(function() {
-  typeText(projectData[1])
-});
-
-$( '#gI3' ).mouseenter(function() {
-  typeText(projectData[2])
-});
-
-$( '#gI4' ).mouseenter(function() {
-  typeText(projectData[3])
-});
-
-$( '#gI5' ).mouseenter(function() {
-  typeText(projectData[4])
-});
-
-$( '#gI6' ).mouseenter(function() {
-  typeText(projectData[5])
-});
-
-// Mouse out event to stop the plugin and clear the screen
-$( '#gI1, #gI2, #gI3, #gI4, #gI5, #gI6' ).mouseleave(function() {
-  $( '#project-description' ).remove();
+// Mouse out event to stop the plugin and clear the screen.
+$('.portfolio-image').mouseleave(function() {
+  // Delete the HTML element holding the description effectively stopping it.
+  $('#project-description').remove();
 });
