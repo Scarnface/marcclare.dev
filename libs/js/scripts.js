4cdbf7c6050612af0fb13b4bc50069d1db2915f3
Scarnface <scarnface@tutanota.com>
2021-07-19 22:36:50 +0100

    Font styles and sizing

--- libs/js/scripts.js
+++ libs/js/scripts.js
@@ -9,7 +9,7 @@
 const galleryImageDataMob = [
   { n: '#gI1', sw: 28.2, sh: 7.2, sx: 5.2, sy: 3.9, d: -0.2 },      // Rick's Garage Sale
   { n: '#gI2', sw: 32.6, sh: 8.55, sx: 59.7, sy: 20.3, d: 0.4 },    // Netmatters
-  { n: '#gI3', sw: 32.2, sh: 8.1, sx: 41.9, sy: 38.2, d: 0 },       // Multi Api
+  { n: '#gI3', sw: 32.2, sh: 8.1, sx: 2.7, sy: 37.1, d: -0.8 },     // Multi Api
   { n: '#gI4', sw: 32.2, sh: 8.1, sx: 61.2, sy: 54.8, d: 0 },       // Music Stache
   { n: '#gI5', sw: 31.8, sh: 8.1, sx: 3.8, sy: 70, d: -0.6 },       // blueJAM
   { n: '#gI6', sw: 31.2, sh: 8.1, sx: 61.4, sy: 88.4, d: 1.2 },     // Codestrips
@@ -18,12 +18,12 @@ const galleryImageDataMob = [
 
 // Position data for info div when in mobile gallery
 const galleryInfoDataMob = [
-  { n: '#project-info', sw: 55, sh: 10, sx: 40, sy: 4.3, d: 0 },  // Dynamic info panel for #gI1
-  { n: '#project-info', sw: 51, sh: 10, sx: 3, sy: 22.3, d: 0 },  // Dynamic info panel for #gI2
-  { n: '#project-info', sw: 38, sh: 14, sx: 3, sy: 36.8, d: 0 },  // Dynamic info panel for #gI3
-  { n: '#project-info', sw: 53, sh: 10, sx: 3, sy: 56, d: 0 },    // Dynamic info panel for #gI4
-  { n: '#project-info', sw: 55, sh: 10, sx: 42, sy: 72, d: 0 },   // Dynamic info panel for #gI5
-  { n: '#project-info', sw: 55, sh: 10, sx: 3, sy: 89, d: 0 }     // Dynamic info panel for #gI6
+  { n: '#project-info', sw: 55, sh: 10, sx: 40, sy: 3.6, d: 0 },  // Dynamic info panel for #gI1
+  { n: '#project-info', sw: 51, sh: 10, sx: 3, sy: 19.8, d: 0 },  // Dynamic info panel for #gI2
+  { n: '#project-info', sw: 55, sh: 14, sx: 40, sy: 35.8, d: 0 }, // Dynamic info panel for #gI3
+  { n: '#project-info', sw: 53, sh: 10, sx: 3, sy: 53.5, d: 0 },    // Dynamic info panel for #gI4
+  { n: '#project-info', sw: 55, sh: 10, sx: 42, sy: 68.5, d: 0 },   // Dynamic info panel for #gI5
+  { n: '#project-info', sw: 55, sh: 10, sx: 3, sy: 86.8, d: 0 }     // Dynamic info panel for #gI6
 ];
 
 // Multi column large background.
@@ -54,7 +54,7 @@ let scaleToBg = array => {
     let newTop = value.sy * bgHeight / 100;
     // Calculate a corresponding font size for the info div.
     if($(window).width() <= 768) {
-      fSize = bgWidth / 25;
+      fSize = bgWidth / 17;
     } else {
       fSize = bgWidth / 40;
     }
@@ -116,35 +116,57 @@ let typeText = obj => {
 
 // Mouse in event to pass the correct text and div position for each image.
 $('.portfolio-image').mouseenter(function() {
-  if( this.id === 'gI1' ) {
-    galleryImageDataMob[6] = galleryInfoDataMob[0];
-    scaleToBg(galleryImageDataMob);
-    typeText(projectData[0]);
-  } else if( this.id === 'gI2' ) {
-    galleryImageDataMob[6] = galleryInfoDataMob[1];
-    scaleToBg(galleryImageDataMob);
-    typeText(projectData[1]);
-  } else if( this.id === 'gI3' ) {
-    galleryImageDataMob[6] = galleryInfoDataMob[2];
-    scaleToBg(galleryImageDataMob);
-    typeText(projectData[2]);
-  } else if( this.id === 'gI4' ) {
-    galleryImageDataMob[6] = galleryInfoDataMob[3];
-    scaleToBg(galleryImageDataMob);
-    typeText(projectData[3]);
-  } else if( this.id === 'gI5' ) {
-    galleryImageDataMob[6] = galleryInfoDataMob[4];
-    scaleToBg(galleryImageDataMob);
-    typeText(projectData[4]);
-  } else if( this.id === 'gI6' ) {
-    galleryImageDataMob[6] = galleryInfoDataMob[5];
-    scaleToBg(galleryImageDataMob);
-    typeText(projectData[5]);
+  if($(window).width() < 768) {
+    // Darken the background and all screens for readability.
+    $('#gallery-background, #gI1, #gI2, #gI3, #gI4, #gI5, #gI6').css('opacity', '0.5');
+    // Undarken the event screen
+    $('#' + this.id).css('opacity', '1');
+    if( this.id === 'gI1' ) {
+      galleryImageDataMob[6] = galleryInfoDataMob[0];
+      scaleToBg(galleryImageDataMob);
+      typeText(projectData[0]);
+    } else if( this.id === 'gI2' ) {
+      galleryImageDataMob[6] = galleryInfoDataMob[1];
+      scaleToBg(galleryImageDataMob);
+      typeText(projectData[1]);
+    } else if( this.id === 'gI3' ) {
+      galleryImageDataMob[6] = galleryInfoDataMob[2];
+      scaleToBg(galleryImageDataMob);
+      typeText(projectData[2]);
+    } else if( this.id === 'gI4' ) {
+      galleryImageDataMob[6] = galleryInfoDataMob[3];
+      scaleToBg(galleryImageDataMob);
+      typeText(projectData[3]);
+    } else if( this.id === 'gI5' ) {
+      galleryImageDataMob[6] = galleryInfoDataMob[4];
+      scaleToBg(galleryImageDataMob);
+      typeText(projectData[4]);
+    } else if( this.id === 'gI6' ) {
+      galleryImageDataMob[6] = galleryInfoDataMob[5];
+      scaleToBg(galleryImageDataMob);
+      typeText(projectData[5]);
+    }
+  } else {
+    if( this.id === 'gI1' ) {
+      typeText(projectData[0]);
+    } else if( this.id === 'gI2' ) {
+      typeText(projectData[1]);
+    } else if( this.id === 'gI3' ) {
+      typeText(projectData[2]);
+    } else if( this.id === 'gI4' ) {
+      typeText(projectData[3]);
+    } else if( this.id === 'gI5' ) {
+      typeText(projectData[4]);
+    } else if( this.id === 'gI6' ) {
+      typeText(projectData[5]);
+    }
   }
 });
 
 // Mouse out event to stop the plugin and clear the screen.
 $('.portfolio-image').mouseleave(function() {
+  // Remove the darkened background effect if in mobile version.
+  $('#gallery-background, #gI1, #gI2, #gI3, #gI4, #gI5, #gI6').css('opacity', '1.0');
   // Delete the HTML element holding the description effectively stopping it.
   $('#project-description').remove();
 });
